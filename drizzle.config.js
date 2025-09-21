1880564395ec8b8ec095109ed6bba81f2af677ca
HUDEVBR <forhudson@hotmail.com>
2025-09-21 09:06:35 -0300

    Gerador de Shorts em vídeo com IA

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/**@type {import ('drizzle-kit').Config} */
+export default {
+    schema: "./configs/schema.js",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: 'postgresql://neondb_owner:npg_xmpNUyofr8H3@ep-lingering-bird-acvgqpde-pooler.sa-east-1.aws.neon.tech/ai-short-video-generator?sslmode=require&channel_binding=require'
+    }
+}
